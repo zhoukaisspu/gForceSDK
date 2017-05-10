38519bc1fa16951fc1e29fa12a067048737bf9f2
zoushimeng <yy220090734@126.com>
2017-05-10 22:26:28 +0800

    Add code for Android platform

---
+++ script/android/BLELibrary/app/src/main/inc/GFBLETypes.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2017, OYMotion Inc.
+ * All rights reserved.
+ *
+ * IMPORTANT: Your use of this Software is limited to those specific rights
+ * granted under the terms of a software license agreement between you and
+ * OYMotion.  You may not use this Software unless you agree to abide by the
+ * terms of the License. The License limits your use, and you acknowledge,
+ * that the Software may not be modified, copied or distributed unless used
+ * solely and exclusively in conjunction with an OYMotion product.  Other
+ * than for the foregoing purpose, you may not use, reproduce, copy, prepare
+ * derivative works of, modify, distribute, perform, display or sell this
+ * Software and/or its documentation for any purpose.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ */
+#ifndef __GF_TYPES_H__
+#define __GF_TYPES_H__
+#pragma once
+
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+#include "gfTypes.h"
+using namespace gf;
+
+typedef void				GF_VOID;
+typedef GF_INT				GF_BOOL;
+#define GF_FALSE			((GF_BOOL)0)
+#define GF_TRUE				(GF_BOOL)(!(GF_FALSE))
+
+#define GF_OK				((GF_INT)0)
+#define GF_FAIL				((GF_INT)1)
+#define GF_SUCCEEDED		(GF_OK == status)
+
+#define BT_ADDRESS_SIZE		6
+#define GF_AUTH_FAIL		0x1000
+
+typedef enum {
+	GF_ERR_CODE_SCAN_BUSY = 0x60,
+	GF_ERR_CODE_NO_RESOURCE = 0x61,
+} GF_ERROR_CODE;
+
+#define BLE_DEVICE_NAME_LENGTH 30
+struct GF_BLEDevice{
+	GF_UINT8   addr_type;
+	GF_UINT8	addr[BT_ADDRESS_SIZE];
+	GF_CHAR	dev_name[BLE_DEVICE_NAME_LENGTH];
+	GF_UINT8   rssi;
+	GF_BLEDevice(GF_UINT8 bytes[], GF_UINT8 type)
+	{
+		for (int i = BT_ADDRESS_SIZE - 1; i >= 0; --i) {
+			this->addr[i] = bytes[i];
+		}
+		addr_type = type;
+	}
+
+};
+
+#define	LOGTYPE_CONSOLE 1
+#define	LOGTYPE_MFC     2
+#define	LOGTYPE_FILE    3
+#define	LOGTYPE_MAX     3
+
+class GF_CCallBack
+{
+public:
+	GF_CCallBack(GF_UINT32 eventmask, GF_UINT8 index) :mEventMask(eventmask), mIndex(index){}
+	virtual GF_STATUS OnDeviceFound(GF_BLEDevice new_device) = 0;
+	virtual GF_STATUS OnEvent(GF_UINT32 event, GF_PUINT8 data, GF_UINT16 length) = 0;
+
+	GF_UINT32 GetEventMask()
+	{
+		return mEventMask;
+	}
+
+	GF_UINT8 GetIndex()
+	{
+		return mIndex;
+	}
+
+private:
+	GF_UINT32 mEventMask;//which event is intent to process.
+	GF_UINT8 mIndex;
+};
+
+typedef enum {
+	State_Idle = 0,
+	State_Scanning = 1,
+	State_Connecting = 2,
+}GF_HubState;
+
+struct GF_ConnectedDevice
+{
+	GF_UINT8 address_type;
+	GF_UINT8 address[BT_ADDRESS_SIZE];
+	GF_UINT16 handle;
+	GF_UINT16 conn_int;
+	GF_UINT16 superTO;
+	GF_UINT16 slavelatency;
+	GF_UINT16 MTUsize;
+};
+#endif
---
+++ script/android/test/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,407 @@
+#include <string>
+#include <android/log.h>
+#include "jni.h"
+#include "../inc/AdapterManagerInterface.h"
+#include "../inc/ClientCallbackInterface.h"
+
+#define TAG "Native-lib"
+#define LOGE(...) __android_log_print( ANDROID_LOG_ERROR, TAG, __VA_ARGS__ )
+#define LOGW(...) __android_log_print( ANDROID_LOG_WARN, TAG, __VA_ARGS__ )
+#define LOGI(...) __android_log_print( ANDROID_LOG_INFO,  TAG, __VA_ARGS__ )
+#define LOGD(...) __android_log_print( ANDROID_LOG_DEBUG, TAG, __VA_ARGS__ )
+
+using namespace std;
+static jclass GlobalClass;
+static jobject GlobalObject;
+JavaVM* globalJavaVM = NULL;
+
+GF_CAdapterManagerInterface* amInterface;
+
+static jmethodID method_onScanResult;
+static jmethodID method_onScanFinished;
+static jmethodID method_onDeviceConnected;
+static jmethodID method_onDeviceDisconnected;
+static jmethodID method_onMTUSizeChanged;
+static jmethodID method_onNotificationReceived;
+
+bool attachEnv(JNIEnv **env, bool* attach) {
+    if (globalJavaVM == NULL) {
+	    LOGE("%s: globalJavaVM is NULL", __FUNCTION__);
+	    return false;
+	}
+
+    if((globalJavaVM != NULL) && (JNI_OK != globalJavaVM->GetEnv(reinterpret_cast<void**>(env),JNI_VERSION_1_6)))
+    {
+        JavaVMAttachArgs args = { JNI_VERSION_1_6, __FUNCTION__, __null };
+        if(JNI_OK != globalJavaVM->AttachCurrentThread(env,&args))
+        {
+            LOGE("%s: AttachCurrentThread failed", __FUNCTION__);
+            return false;
+        }
+        *attach = true;
+    } else {
+        LOGE("%s: GetEnv OK!", __FUNCTION__);
+        *attach = false;
+    }
+
+    return true;
+}
+
+class Callback : public GF_CClientCallback
+{
+public:
+	void onScanResult(GF_BLEDevice* device) 
+	{
+	    LOGD("%s:", __FUNCTION__);
+		jbyteArray addr = NULL, devname = NULL;
+		bool bAttached = false;
+
+		if (method_onScanResult != NULL) {
+            if (false == attachEnv(&env, &bAttached)) {
+                return ;
+            }
+
+            addr = env->NewByteArray(BT_ADDRESS_SIZE);
+    		if (addr == NULL) goto Fail;
+    		env->SetByteArrayRegion(addr, 0, BT_ADDRESS_SIZE, (jbyte *)device->addr);
+
+            int name_len = strlen(device->dev_name);
+    		devname = env->NewByteArray(name_len);
+    		if (devname == NULL) goto Fail;
+			env->SetByteArrayRegion(devname, 0, name_len, (jbyte*)device->dev_name);
+
+            if (env != NULL && GlobalObject != NULL) {
+                env->CallVoidMethod(GlobalObject, method_onScanResult, addr, devname, device->rssi);
+                //env->CallVoidMethod(GlobalObject, method_onScanFinished);
+            }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+
+		    env->DeleteLocalRef(addr);
+            env->DeleteLocalRef(devname);
+
+		    return;
+		}
+		
+Fail:
+	    if (addr) env->DeleteLocalRef(addr);
+	    if (devname) env->DeleteLocalRef(devname);
+	    if(bAttached) {
+        	globalJavaVM->DetachCurrentThread();
+        }
+	}
+
+	void onScanFinished()
+	{
+	    LOGD("%s:", __FUNCTION__);
+		bool bAttached = false;
+		
+		if (method_onScanResult != NULL) {
+	        if (false == attachEnv(&env, &bAttached)) {
+	            return ;
+	        }
+
+	        if (env != NULL && GlobalObject != NULL) {
+	            env->CallVoidMethod(GlobalObject, method_onScanFinished);
+	        }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+		}
+	}
+
+	void onDeviceDisconnected(GF_STATUS status, GF_ConnectedDevice *device, GF_UINT8 reason)
+	{
+	    LOGD("%s:", __FUNCTION__);
+		jbyteArray addr = NULL;
+		bool bAttached = false;
+
+		if (method_onDeviceDisconnected != NULL) {
+            if (false == attachEnv(&env, &bAttached)) {
+                return ;
+            }
+
+            addr = env->NewByteArray(BT_ADDRESS_SIZE);
+    		if (addr == NULL) goto Fail;
+    		env->SetByteArrayRegion(addr, 0, BT_ADDRESS_SIZE, (jbyte *)device->address);
+            
+            if (env != NULL && GlobalObject != NULL) {
+                env->CallVoidMethod(GlobalObject, method_onDeviceDisconnected, addr, device->handle, reason);
+            }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+
+		    env->DeleteLocalRef(addr);
+
+		    return;
+		}
+
+Fail:
+	    if (addr) env->DeleteLocalRef(addr);
+	    if(bAttached) {
+        	globalJavaVM->DetachCurrentThread();
+        }
+	}
+
+	void onDeviceConnected(GF_STATUS status, GF_ConnectedDevice *device)
+	{
+	    LOGD("%s:", __FUNCTION__);
+		jbyteArray addr = NULL;
+		bool bAttached = false;
+
+		if (method_onDeviceConnected != NULL) {
+            if (false == attachEnv(&env, &bAttached)) {
+                return ;
+            }
+
+            addr = env->NewByteArray(BT_ADDRESS_SIZE);
+    		if (addr == NULL) goto Fail;
+    		env->SetByteArrayRegion(addr, 0, BT_ADDRESS_SIZE, (jbyte *)device->address);
+            
+            if (env != NULL && GlobalObject != NULL) {
+                env->CallVoidMethod(GlobalObject, method_onDeviceConnected, addr, device->handle);
+            }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+
+		    env->DeleteLocalRef(addr);
+
+		    return;
+		}
+
+Fail:
+	    if (addr) env->DeleteLocalRef(addr);
+	    if(bAttached) {
+        	globalJavaVM->DetachCurrentThread();
+        }
+	}
+
+	void onMTUSizeChanged(GF_STATUS status, GF_UINT16 handle, GF_UINT16 mtu_size)
+	{
+	    LOGD("%s:", __FUNCTION__);
+		bool bAttached = false;
+
+		if (method_onMTUSizeChanged != NULL) {
+            if (false == attachEnv(&env, &bAttached)) {
+                return ;
+            }
+
+            if (env != NULL && GlobalObject != NULL) {
+                env->CallVoidMethod(GlobalObject, method_onMTUSizeChanged, handle, mtu_size);
+            }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+
+		    return;
+		}
+	}
+
+	/*Not support on Android platform, because connection parameter will not be accessable to upper level.*/
+	void onConnectionParmeterUpdated(GF_STATUS status, GF_UINT16 handle, GF_UINT16 conn_int, GF_UINT16 superTO, GF_UINT16 slavelatency)
+	{
+		return;
+
+	}
+
+	/*Not support for now, because characteristic read with different format on window and android platform.*/
+	void onCharacteristicValueRead(GF_STATUS status, GF_UINT16 handle, GF_UINT8 length, GF_PUINT8 data)
+	{
+		return;
+	}
+
+	void onNotificationReceived(GF_UINT16 handle, GF_UINT8 length, GF_PUINT8 data)
+	{
+	    LOGD("%s:", __FUNCTION__);
+		jbyteArray notification = NULL;
+		bool bAttached = false;
+
+		if (method_onNotificationReceived != NULL) {
+            if (false == attachEnv(&env, &bAttached)) {
+                return ;
+            }
+
+            notification = env->NewByteArray(length);
+    		if (notification == NULL) goto Fail;
+    		env->SetByteArrayRegion(notification, 0, BT_ADDRESS_SIZE, (jbyte *)data);
+            
+            if (env != NULL && GlobalObject != NULL) {
+                env->CallVoidMethod(GlobalObject, method_onNotificationReceived, handle, notification);
+            }
+
+			if(bAttached) {
+				globalJavaVM->DetachCurrentThread();
+		    }
+
+		    env->DeleteLocalRef(notification);
+
+		    return;
+		}
+
+Fail:
+	    if (notification) env->DeleteLocalRef(notification);
+	    if(bAttached) {
+        	globalJavaVM->DetachCurrentThread();
+        }
+	}
+
+	/*No need to be supported on Android platform.*/
+	void onComDestory()
+	{
+		return;
+	}
+
+private:
+    JNIEnv *env;
+};
+
+Callback* globalCallback;
+
+static void classInitNative(JNIEnv* env, jclass clazz) {
+    LOGD("%s:", __FUNCTION__);
+	jclass jniCallbackClass = env->FindClass("com/oym/test/MainActivity");
+	method_onScanResult = env->GetMethodID(jniCallbackClass, "onScanResult", "([B[BI)V");
+	method_onScanFinished = env->GetMethodID(jniCallbackClass, "onScanFinished", "()V");
+	method_onDeviceConnected = env->GetMethodID(jniCallbackClass, "onDeviceConnected", "([BI)V");
+	method_onDeviceDisconnected = env->GetMethodID(jniCallbackClass, "onDeviceDisconnected", "([BIB)V");
+	method_onMTUSizeChanged = env->GetMethodID(jniCallbackClass, "onMTUSizeChanged", "(II)V");
+	method_onNotificationReceived = env->GetMethodID(jniCallbackClass, "onNotificationReceived", "(I[B)V");
+}
+
+static bool initNative(JNIEnv* env, jobject object) {
+     LOGD("%s:", __FUNCTION__);
+     amInterface = GF_CAdapterManagerInterface::GetInstance();
+	 globalCallback = new Callback;
+     if (amInterface != NULL) {
+        if (GF_FAIL == amInterface->Init(0, 0)) {
+			LOGD("%s:Init interface fail", __FUNCTION__);
+			return JNI_FALSE;
+		} else {
+		    LOGD("%s: RegisterClientCallback ", __FUNCTION__);
+			amInterface->RegisterClientCallback(globalCallback);
+			GlobalObject = env->NewGlobalRef(object);
+		}
+     }
+
+     return JNI_TRUE;
+}
+
+static bool cleanupNative(JNIEnv* env, jobject obj) {
+    LOGD("%s:", __FUNCTION__);
+    env->DeleteGlobalRef(GlobalObject);
+    GlobalObject = NULL;
+    return JNI_TRUE;
+}
+
+static bool startScanNative(JNIEnv* env, jobject object) {
+    LOGD("%s:", __FUNCTION__);
+    //env->CallBooleanMethod(GlobalObject, method_StartScan);
+    if (amInterface != NULL) {
+        amInterface->StartScan(0);
+    }
+    return JNI_TRUE;
+}
+
+static bool stopScanNative(JNIEnv* env, jobject object) {
+    LOGD("%s:", __FUNCTION__);
+    if (amInterface != NULL) {
+        amInterface->StopScan();
+    }
+
+    return JNI_TRUE;
+}
+
+static bool connectNative(JNIEnv* env, jobject object, jbyteArray addr) {
+    LOGD("%s:", __FUNCTION__);
+    if (amInterface != NULL) {
+        jbyte* addressArray = env->GetByteArrayElements(addr, 0);
+        amInterface->Connect((GF_PUINT8)addressArray, 0, 0);
+        env->ReleaseByteArrayElements(addr, addressArray, JNI_ABORT);
+    } else {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
+
+static bool cancelConnectNative( JNIEnv* env, jobject obj, jbyteArray addr) {
+    LOGD("%s:", __FUNCTION__);
+    if (amInterface != NULL) {
+        jbyte* addressArray = env->GetByteArrayElements(addr, 0);
+        amInterface->CancelConnect((GF_PUINT8)addressArray, 0);
+        env->ReleaseByteArrayElements(addr, addressArray, JNI_ABORT);
+    } else {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
+
+static bool disconnectNative( JNIEnv* env, jobject obj, jint handle) {
+    LOGD("%s:", __FUNCTION__);
+    if (amInterface != NULL) {
+        amInterface->Disconnect((GF_UINT16)(handle & 0xFFFF));
+    } else {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
+
+static jstring stringFromJNINative( JNIEnv* env, jobject obj) {
+    LOGD("%s:", __FUNCTION__);
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF("111 Hello from C++ 22...");
+}
+
+static JNINativeMethod sMethods[] = {
+    /* name,            signature,         funcPtr */
+    {"classInitNative", "()V", (void *) classInitNative},
+    {"initNative", "()Z", (void *) initNative},
+    {"startScanNative", "()Z", (void *) startScanNative},
+    {"stopScanNative", "()Z", (void *) stopScanNative},
+    {"cleanupNative", "()V", (void*) cleanupNative},
+    {"stringFromJNI", "()Ljava/lang/String;", (void*)stringFromJNINative},
+    {"connectNative", "([B)Z", (void*) connectNative},
+    {"cancelConnectNative", "([B)Z", (void*) cancelConnectNative},
+    {"disconnectNative", "(I)Z", (void*) disconnectNative},
+};
+
+
+/*
+ * JNI Initialization
+ */
+jint JNI_OnLoad(JavaVM *jvm, void *reserved)
+{
+    JNIEnv *env;
+    int status;
+    jclass cls;
+
+    LOGD("Native-Lib : loading JNI\n");
+    globalJavaVM = jvm;
+
+    // Check JNI version
+    if (jvm->GetEnv((void **)&env, JNI_VERSION_1_6)) {
+        LOGE("JNI version mismatch error");
+        return JNI_ERR;
+    }
+
+    cls = env->FindClass("com/oym/test/MainActivity");
+    if (cls == NULL)
+    {
+        return JNI_ERR;
+    }
+
+    /*Register native function to java code.*/
+    env->RegisterNatives(cls, sMethods, sizeof(sMethods) / sizeof(sMethods[0]));
+
+    return JNI_VERSION_1_6;
+}
